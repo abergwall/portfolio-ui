4e9866b70d5e569b930b0e82606fe5d206c1c06a
abergwall <antonbergwall@gmail.com>
2022-02-25 23:42:27 +0100

    renamed and exposed socialrow

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Container, Row, Column } from "./LayoutComponents";
 export { PageWrapper } from "./PageWrapper";
 export { MenuButton } from "./MenuButton";
 export { RoundImage } from "./RoundImage";
+export { SocialRow } from "./SocialRow";
